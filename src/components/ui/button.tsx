a21e4bed63e87924ced96010317f60eff7dabc41
HARISH K BTech_CyS_IoT <e0222015@sret.edu.in>
2025-09-25 03:25:16 +0000

    in this there is issus that the user click any tab the tab open double o

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -42,11 +42,18 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (asChild) {
+        e.stopPropagation();
+      }
+      props.onClick?.(e);
+    }
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
+        onClick={handleClick}
       />
     )
   }
